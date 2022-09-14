f985da68dd0cb46d2470f36c7c39b3d2aeb335b3
thraneh <thraneh@gmail.com>
2022-09-14 09:26:20 +0200

    update

--- src/roq/udp_subscriber/parser.cpp
+++ src/roq/udp_subscriber/parser.cpp
@@ -19,39 +19,40 @@ namespace udp_subscriber {
 size_t Parser::dispatch(
     Handler &handler, std::span<std::byte const> const &buffer, TraceInfo const &trace_info, Shared &shared) {
   using Frame = core::udp::Frame;
-  if (std::size(buffer) > sizeof(Frame)) {
-    Frame frame;
-    std::memcpy(&frame, std::data(buffer), sizeof(frame));
-    if (frame.magic == core::udp::MAGIC) {
-      // ...
-      if (frame.total_fragments == 1 && frame.fragment_number == 0) {
-        auto payload = buffer.subspan(sizeof(frame));
-        if (!std::empty(payload)) {
-          switch (frame.encoding) {
-            using enum core::udp::Encoding;
-            case UNDEFINED:
-              break;
-            case NATIVE:
-              break;
-            case FLATBUFFERS:
-              FBSParser::dispatch_helper(handler, payload, trace_info, shared, frame);
-              break;
-            case JSON:
-              JSONParser::dispatch_helper(handler, payload, trace_info, shared, frame);
-              break;
-          }
-        } else {
-          // heartbeat
-        }
-        return std::size(buffer);
-      } else {
-        log::fatal("Unexpected: fragments"sv);
+  if (std::size(buffer) < sizeof(Frame)) {
+    log::warn("Unexpected: len(data)={} < len(frame)={}"sv, std::size(buffer), sizeof(Frame));
+    return 0;
+  }
+  Frame frame;
+  std::memcpy(&frame, std::data(buffer), sizeof(frame));
+  if (frame.magic != core::udp::MAGIC) {
+    log::warn(R"(Unexpected: magic=\x{:02x} != \x{:02x})"sv, frame.magic, core::udp::MAGIC);
+    return 0;
+  }
+  // ...
+  if (frame.total_fragments == 0 && frame.fragment_number == 0) {
+    auto payload = buffer.subspan(sizeof(frame));
+    if (!std::empty(payload)) {
+      switch (frame.encoding) {
+        using enum core::udp::Encoding;
+        case UNDEFINED:
+          break;
+        case NATIVE:
+          break;
+        case FLATBUFFERS:
+          FBSParser::dispatch_helper(handler, payload, trace_info, shared, frame);
+          break;
+        case JSON:
+          JSONParser::dispatch_helper(handler, payload, trace_info, shared, frame);
+          break;
       }
     } else {
-      log::warn(R"(Unexpected: magic=\x{:02x} != \x{:02x})"sv, frame.magic, core::udp::MAGIC);
+      // heartbeat
+      log::info<1>("HEARTBEAT"sv);
     }
+    return std::size(buffer);
   } else {
-    log::warn("Unexpected: len(data)={} < len(frame)={}"sv, std::size(buffer), sizeof(Frame));
+    log::fatal("Unexpected: fragments"sv);
   }
   return 0;
 }
